ef86dd3468b41b69e2e09f5210bb87f155de78e7
yd2676 <121277205+yd2676@users.noreply.github.com>
2023-12-15 16:33:33 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,9 @@
-from app import main
+from fastapi.testclient import TestClient
+
+from app.main import app
+
+client = TestClient(app)
 
 def test_index():
-    client = main.TestClient(app)
     response = client.get('/')
     assert response.status_code == 200
