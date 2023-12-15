43cb6cf588fb877c836aa645d83c98fba919f5fe
yd2676 <121277205+yd2676@users.noreply.github.com>
2023-12-15 16:21:00 -0500

    Update test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,6 +1,6 @@
 from app import main
 
 def test_index():
-    client = main.test_client()
-    response = client.get('/posts')
+    client = main.TestClient()
+    response = client.get('/')
     assert response.status_code == 200
