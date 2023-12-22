affef06915ea37419332a0b72262bdd33c297c46
yd2676 <121277205+yd2676@users.noreply.github.com>
2023-12-22 18:25:15 -0500

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from typing import List, Optional, Dict
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Form
 from fastapi.encoders import jsonable_encoder
 from pydantic import BaseModel
 import uvicorn
@@ -43,7 +43,6 @@ class MySQLDataService:
             conn = self._get_connection()
             cur = conn.cursor()
             full_sql = cur.mogrify(sql)
-            print("Full SQL = ", full_sql)
             # res = cur.execute(sql)
             cur.execute(sql)
             result = cur.fetchall()
@@ -72,7 +71,6 @@ class MySQLDataService:
             conn = self._get_connection()
             cur = conn.cursor()
             full_sql = cur.mogrify(sql, (post_id,))
-            print("Full SQL = ", full_sql)
             cur.execute(sql, (post_id,))
             result = cur.fetchall()
             conn.close()
@@ -138,18 +136,27 @@ class MySQLDataService:
                 conn.close()
             return None
 
-    def update_post(self, post_id, updated_author=None, updated_author_id=None, updated_content=None):
+    def update_post(self, post_id, author, author_id, content):
         conn = None
         try:
+            conn = self._get_connection()
+            cur = conn.cursor()
+            check_sql = "SELECT author_id FROM forums.posts WHERE post_id = %s"
+            cur.execute(check_sql, (post_id,))
+            result = cur.fetchall()
+            if not result[0]:
+                raise Exception("Post not found")
+            if result[0]['author_id'] != author_id:
+                print(111)
+                raise Exception("User is not the author of the post")
+
             update_sql = """
                 UPDATE forums.posts
                 SET author = %s, author_id = %s, content = %s
                 WHERE post_id = %s
                 """
-            conn = self._get_connection()
-            cur = conn.cursor()
-            cur.execute(update_sql, (
-            updated_author, updated_author_id, updated_content, post_id))
+
+            cur.execute(update_sql, (author, author_id, content, post_id))
 
             if cur.rowcount == 0:
                 raise Exception("Post not found or no change in data")
@@ -173,17 +180,27 @@ class MySQLDataService:
                 conn.close()
             return None
 
-    def delete_comment(self, post_id, comment_id):
+    def delete_comment(self, post_id, comment_id, commenter_id):
         conn = None
         try:
-            sql = "DELETE FROM forums.comments WHERE post_id = %s and comment_id = %s"
             conn = self._get_connection()
             cur = conn.cursor()
+
+            check_sql = "SELECT commenter_id FROM forums.comments WHERE comment_id = %s"
+            cur.execute(check_sql, (comment_id,))
+            result = cur.fetchall()
+            if not result[0]:
+                raise Exception("Post not found")
+            if result[0]['commenter_id'] != commenter_id:
+                raise Exception("User is not the commenter of the post")
+
+            sql = "DELETE FROM forums.comments WHERE post_id = %s and comment_id = %s"
+
             cur.execute(sql, (post_id, comment_id))
 
             # Check if the comment was actually deleted
             if cur.rowcount == 0:
-                raise Exception("Comment not found or already deleted")
+                raise Exception("Comment not found")
 
             conn.commit()
             conn.close()
@@ -195,22 +212,30 @@ class MySQLDataService:
                 conn.close()
             return None
 
-    def delete_post(self, post_id):
+    def delete_post(self, post_id, author_id):
         conn = None
         try:
-            delete_comments_sql = "DELETE FROM forums.comments WHERE post_id = %s"
-
-            delete_post_sql = "DELETE FROM forums.posts WHERE post_id = %s"
-
             conn = self._get_connection()
             cur = conn.cursor()
 
+            # check if user is the author of the post
+            check_sql = "SELECT author_id FROM forums.posts WHERE post_id = %s"
+            cur.execute(check_sql, (post_id,))
+            result = cur.fetchall()
+            if not result[0]:
+                raise Exception("Post not found")
+            if result[0]['author_id'] != author_id:
+                raise Exception("User is not the author of the post")
+
+            delete_comments_sql = "DELETE FROM forums.comments WHERE post_id = %s"
             cur.execute(delete_comments_sql, (post_id,))
 
+
+            delete_post_sql = "DELETE FROM forums.posts WHERE post_id = %s"
             cur.execute(delete_post_sql, (post_id,))
 
             if cur.rowcount == 0:
-                raise Exception("Post not found or already deleted")
+                raise Exception("Post not found")
 
             conn.commit()
             conn.close()
@@ -222,7 +247,6 @@ class MySQLDataService:
                 conn.close()
             return None
 
-# In-memory structure to store posts
 class Comment(BaseModel):
     commenter: str
     commenter_id: int
@@ -260,12 +284,12 @@ def send_message_to_sns_topic(topic_arn, message, subject=None):
 
 my_sql_data_service = MySQLDataService()
 
-#topic_arn = "arn:aws:sns:us-east-2:538930638837:Send_email"
-topic_arn = "arn:aws:sns:us-east-2:538930638837:send_example"
+topic_arn = "arn:aws:sns:us-east-2:538930638837:Send_email"
+#topic_arn = "arn:aws:sns:us-east-2:538930638837:send_example"
 
 @app.get("/")
 async def read_main():
-    return {"Docker Microservice: Version 2.0"}
+    return {"Docker Microservice: Version 1.0"}
 
 @app.get("/posts")
 async def get_all_posts():
@@ -316,8 +340,8 @@ async def get_post(post_id: int):
     return jsonable_encoder(formatted_data)
 
 @app.post("/posts", response_model=Post)
-async def create_post(post: Post):
-    post_id, result = my_sql_data_service.create_post(post.author, post.author_id, post.content)
+async def create_post(content: str, author: str = Form(), author_id: int = Form()):
+    post_id, result = my_sql_data_service.create_post(author, author_id, content)
     subject_text = f'A New Post(Post_id: {post_id}) was Posted'
     message_text = str(result[0])
     send_message_to_sns_topic(topic_arn, message_text, subject_text)
@@ -325,8 +349,8 @@ async def create_post(post: Post):
 
 
 @app.post("/posts/{post_id}/comments", response_model=Comment)
-async def create_comment(post_id: int, comment: Comment):
-    comment_id, result = my_sql_data_service.create_comment(post_id, comment.commenter, comment.commenter_id, comment.content)
+async def create_comment(post_id: int, content: str, commenter: str = Form(), commenter_id: int = Form()):
+    comment_id, result = my_sql_data_service.create_comment(post_id, commenter, commenter_id, content)
 
     subject_text = f'A New Comment(Comment_id: {comment_id}) was Made to Post(Post_id: {post_id})'
     message_text = str(result[0])
@@ -335,13 +359,10 @@ async def create_comment(post_id: int, comment: Comment):
 
 
 @app.put("/posts/{post_id}")
-async def update_post(post_id: int, updated_post: Post):
-    result = my_sql_data_service.update_post(post_id, updated_post.author, updated_post.author_id, updated_post.content)
-
-    subject_text = f'Post(Post_id: {post_id}) was Updated'
-    message_text = str(updated_post)
-    send_message_to_sns_topic(topic_arn, message_text, subject_text)
-    print(result)
+async def update_post(post_id: int, content: str, author: str = Form(), author_id: int = Form()):
+    result = my_sql_data_service.update_post(post_id, author, author_id, content)
+    if not result:
+        return "Post not found or user is not the author of the post"
     formatted_data = {}
 
     for item in result:
@@ -361,11 +382,16 @@ async def update_post(post_id: int, updated_post: Post):
             "commenter_id": item["commenter_id"],
             "content": item["c.content"]
         })
+    subject_text = f'Post(Post_id: {post_id}) was Updated'
+    message_text = str(formatted_data)
+    send_message_to_sns_topic(topic_arn, message_text, subject_text)
     return jsonable_encoder(formatted_data)
 
 @app.delete("/posts/{post_id}")
-async def delete_post(post_id: int):
-    result = my_sql_data_service.delete_post(post_id)
+async def delete_post(post_id: int, author_id: int = Form()):
+    result = my_sql_data_service.delete_post(post_id, author_id)
+    if not result:
+        return "Post not found or user is not the author of the post"
 
     subject_text = f'Post(Post_id: {post_id}) was Deleted'
     message_text = f'Post(Post_id: {post_id}) was Deleted'
@@ -374,9 +400,10 @@ async def delete_post(post_id: int):
 
 
 @app.delete("/posts/{post_id}/comments/{comment_id}")
-async def delete_comment(post_id: int, comment_id: str):
-    result = my_sql_data_service.delete_comment(post_id, comment_id)
-
+async def delete_comment(post_id: int, comment_id: int, commenter_id: int = Form()):
+    result = my_sql_data_service.delete_comment(post_id, comment_id, commenter_id)
+    if not result:
+        return "Comment not found or user is not the commenter of the comment"
     subject_text = f'Comment(Comment_id: {comment_id}) to Post(Post_id: {post_id}) was Deleted'
     message_text = f'Comment(Comment_id: {comment_id}) to Post(Post_id: {post_id}) was Deleted'
     send_message_to_sns_topic(topic_arn, message_text, subject_text)
